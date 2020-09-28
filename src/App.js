9dfab92d34de80c05c5e4a7cc3ae473f3b7adcdc
fastsly <szalai.laszlo.istvan.psiho@gmail.com>
2020-09-28 13:48:43 +0300

    added field for url input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
+import ImgLinkForm from './components/ImgLinkForm/ImgLinkForm.js'
+import Rank from './components/Rank/Rank.js'
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOptions ={
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    },
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    size: {
+        value: 2
+    }
+  },
+  interactivity: {
+      events: {
+          onhover: {
+              enable: true,
+              mode: "repulse"
+          }
+      }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles' params={particlesOptions}/>
       <Navigation/>
       <Logo/>
-      {/* <ImgFormLink/>
-      <FaceRecognition/> */}
+      <Rank/>
+      <ImgLinkForm/>	    
+      {/* <FaceRecognition/> */}
     </div>
   );
 }
